cd3b4438cf7511dc0da85c948f089f59f319f780
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2023-11-02 14:59:12 +0100

    Update dsi_panel_c3s_36_0f_0c_fhdp_video.c

--- drivers/misc/mediatek/lcm/dsi_panel_c3s_36_0f_0c_fhdp_video/dsi_panel_c3s_36_0f_0c_fhdp_video.c
+++ drivers/misc/mediatek/lcm/dsi_panel_c3s_36_0f_0c_fhdp_video/dsi_panel_c3s_36_0f_0c_fhdp_video.c
@@ -85,13 +85,13 @@ extern int tps65132_write_bytes(unsigned char addr, unsigned char value);
 /* static unsigned char lcd_id_pins_value = 0xFF; */
 static const unsigned char LCD_MODULE_ID = 0x01;
 #define LCM_DSI_CMD_MODE                                    0
-#define FRAME_WIDTH                                     (1440)
-#define FRAME_HEIGHT                                    (3200)
+#define FRAME_WIDTH                                     (720)
+#define FRAME_HEIGHT                                    (1600)
 
 /* physical size in um */
 #define LCM_PHYSICAL_WIDTH (67932)
 #define LCM_PHYSICAL_HEIGHT (150960)
-#define LCM_DENSITY	(160)
+#define LCM_DENSITY	(320)
 
 
 #define GPIO_LCD_BIAS_ENP_PIN 168
