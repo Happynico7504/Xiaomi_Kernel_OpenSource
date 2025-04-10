b975b3143038812fdc2bb751a73e6eba84a8b05f
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-10 19:55:02 +0200

    Update mtk_cpufreq_opp_table.h

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6761/mtk_cpufreq_opp_table.h
@@ -25,22 +25,22 @@
 #define CPU_DVFS_FREQ14_LL_FY    50000     /* KHz */
 #define CPU_DVFS_FREQ15_LL_FY    25000     /* KHz */
 
-#define CPU_DVFS_VOLT0_VPROC_LL_FY    110000           /* 10uV */
-#define CPU_DVFS_VOLT1_VPROC_LL_FY    100000           /* 10uV */
-#define CPU_DVFS_VOLT2_VPROC_LL_FY    900000           /* 10uV */
-#define CPU_DVFS_VOLT3_VPROC_LL_FY     75000           /* 10uV */
-#define CPU_DVFS_VOLT4_VPROC_LL_FY     91250           /* 10uV */
-#define CPU_DVFS_VOLT5_VPROC_LL_FY     88750           /* 10uV */
-#define CPU_DVFS_VOLT6_VPROC_LL_FY     85000           /* 10uV */
-#define CPU_DVFS_VOLT7_VPROC_LL_FY     82500           /* 10uV */
-#define CPU_DVFS_VOLT8_VPROC_LL_FY     80000           /* 10uV */
-#define CPU_DVFS_VOLT9_VPROC_LL_FY     77500           /* 10uV */
-#define CPU_DVFS_VOLT10_VPROC_LL_FY    75000           /* 10uV */
-#define CPU_DVFS_VOLT11_VPROC_LL_FY    72500           /* 10uV */
-#define CPU_DVFS_VOLT12_VPROC_LL_FY    70625           /* 10uV */
-#define CPU_DVFS_VOLT13_VPROC_LL_FY    50000           /* 10uV */
-#define CPU_DVFS_VOLT14_VPROC_LL_FY    45000           /* 10uV */
-#define CPU_DVFS_VOLT15_VPROC_LL_FY    40000           /* 10uV */
+#define CPU_DVFS_VOLT0_VPROC_LL_FY    120000           /* 10uV */
+#define CPU_DVFS_VOLT1_VPROC_LL_FY    110000           /* 10uV */
+#define CPU_DVFS_VOLT2_VPROC_LL_FY    100000           /* 10uV */
+#define CPU_DVFS_VOLT3_VPROC_LL_FY     90000           /* 10uV */
+#define CPU_DVFS_VOLT4_VPROC_LL_FY     75000           /* 10uV */
+#define CPU_DVFS_VOLT5_VPROC_LL_FY     75000           /* 10uV */
+#define CPU_DVFS_VOLT6_VPROC_LL_FY     50000           /* 10uV */
+#define CPU_DVFS_VOLT7_VPROC_LL_FY     50000           /* 10uV */
+#define CPU_DVFS_VOLT8_VPROC_LL_FY     50000           /* 10uV */
+#define CPU_DVFS_VOLT9_VPROC_LL_FY     30000           /* 10uV */
+#define CPU_DVFS_VOLT10_VPROC_LL_FY    30000           /* 10uV */
+#define CPU_DVFS_VOLT11_VPROC_LL_FY    30000           /* 10uV */
+#define CPU_DVFS_VOLT12_VPROC_LL_FY    25000           /* 10uV */
+#define CPU_DVFS_VOLT13_VPROC_LL_FY    25000           /* 10uV */
+#define CPU_DVFS_VOLT14_VPROC_LL_FY    15000           /* 10uV */
+#define CPU_DVFS_VOLT15_VPROC_LL_FY    15000           /* 10uV */
 
 /* SB */
 /* for DVFS OPP table LL */
