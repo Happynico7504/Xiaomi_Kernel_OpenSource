67d54f768f56eaa864d1d08451e54d3a051fa124
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-11 12:49:38 +0200

    Create fbflush.c

---
+++ drivers/misc/nicochristmann/fbflush/fbflush.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/slab.h>
+
+#include "../mediatek/video/mt6765/videox/primary_display.h"
+
+static struct timer_list fbflush_timer;
+
+static void fbflush_callback(struct timer_list *t)
+{
+    primary_display_trigger(true, NULL, 0);
+    mod_timer(&fbflush_timer, jiffies + msecs_to_jiffies(10));
+}
+
+static int __init fbflush_init(void)
+{
+    pr_info("fbflush: starting display refresh timer\n");
+    timer_setup(&fbflush_timer, fbflush_callback, 0);
+    mod_timer(&fbflush_timer, jiffies + msecs_to_jiffies(1000));
+    return 0;
+}
+
+static void __exit fbflush_exit(void)
+{
+    del_timer_sync(&fbflush_timer);
+    pr_info("fbflush: stopped\n");
+}
+
+module_init(fbflush_init);
+module_exit(fbflush_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nico Christmann");
+MODULE_DESCRIPTION("In-tree MTK framebuffer refresher using primary_display_trigger");
