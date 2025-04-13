3d6571681db4fcec4b573b4afb5aea14b8140b76
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-13 02:18:49 +0200

    Update fbflush.c

--- drivers/misc/nicochristmann/fbflush/fbflush.c
+++ drivers/misc/nicochristmann/fbflush/fbflush.c
@@ -1,31 +1,30 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/timer.h>
+#include <linux/workqueue.h>
 #include <linux/jiffies.h>
-#include <linux/slab.h>
 
 extern int primary_display_trigger(bool blocking, void *callback, unsigned int userdata);
 
-static struct timer_list fbflush_timer;
+static struct delayed_work fbflush_work;
 
-static void fbflush_callback(struct timer_list *t)
+static void fbflush_work_func(struct work_struct *work)
 {
-    primary_display_trigger(1, NULL, 0);
-    mod_timer(&fbflush_timer, jiffies + msecs_to_jiffies(10));
+    primary_display_trigger(true, NULL, 0);
+    schedule_delayed_work(&fbflush_work, msecs_to_jiffies(10));
 }
 
 static int __init fbflush_init(void)
 {
-    pr_info("fbflush: starting display refresh timer\n");
-    timer_setup(&fbflush_timer, fbflush_callback, 0);
-    mod_timer(&fbflush_timer, jiffies + msecs_to_jiffies(1000));
+    pr_info("fbflush: scheduling display refresh work\n");
+    INIT_DELAYED_WORK(&fbflush_work, fbflush_work_func);
+    schedule_delayed_work(&fbflush_work, msecs_to_jiffies(1000));
     return 0;
 }
 
 static void __exit fbflush_exit(void)
 {
-    del_timer_sync(&fbflush_timer);
+    cancel_delayed_work_sync(&fbflush_work);
     pr_info("fbflush: stopped\n");
 }
 
