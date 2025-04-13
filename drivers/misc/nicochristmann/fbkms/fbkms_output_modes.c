df89ba064bb0af454ec82a533751e6189922fcb1
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-13 14:48:14 +0200

    Update fbkms_output_modes.c

--- drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
@@ -12,12 +12,12 @@ struct drm_display_mode fbkms_preferred_mode = {
     .name = "720x1600"
 };
 
+EXPORT_SYMBOL(fbkms_preferred_mode);
+
 // Mode setup function used by fbkms.c
 void fbkms_setup_mode(struct fbkms_device *fbkms)
 {
     fbkms->mode = fbkms_preferred_mode;
 }
 
-
-EXPORT_SYMBOL(fbkms_preferred_mode);
 EXPORT_SYMBOL(fbkms_setup_mode);
