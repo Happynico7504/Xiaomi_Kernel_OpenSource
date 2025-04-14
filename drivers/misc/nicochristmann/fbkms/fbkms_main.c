b71e2b2d9d2841b1fb4ade713c6a8e86f962ff31
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-14 23:18:35 +0200

    Update fbkms_main.c

--- drivers/misc/nicochristmann/fbkms/fbkms_main.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_main.c
@@ -17,6 +17,7 @@ static void fbkms_pipe_enable(struct drm_simple_display_pipe *pipe,
   
     struct fbkms_device *fbkms = container_of(pipe->crtc.dev, struct fbkms_device, drm);
 
+    struct drm_framebuffer *fb = plane_state->fb;
 
     struct drm_gem_cma_object *cma_obj = to_drm_gem_cma_obj(fb->obj[0]);
     void *src = cma_obj->vaddr;
