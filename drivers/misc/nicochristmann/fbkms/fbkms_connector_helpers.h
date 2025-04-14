5e6e6baef829fa4c02945a27b2c04efb270e02b3
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-14 16:47:24 +0200

    Update fbkms_connector_helpers.h

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
@@ -8,25 +8,23 @@
 static inline int fbkms_get_modes(struct drm_connector *connector,
                                   struct drm_display_mode *preferred_mode)
 {
-    struct drm_display_mode *mode;
-
-    mode = drm_mode_duplicate(connector->dev, preferred_mode);
+    struct drm_display_mode *mode = drm_mode_duplicate(connector->dev, preferred_mode);
     if (!mode)
         return 0;
 
+    mode->type |= DRM_MODE_TYPE_PREFERRED;
     drm_mode_probed_add(connector, mode);
     connector->display_info.width_mm = 68;
     connector->display_info.height_mm = 136;
+
     return 1;
 }
 
-
 static int fbkms_get_modes_wrapper(struct drm_connector *connector)
 {
-    extern struct drm_display_mode fbkms_preferred_mode; // Defined in your modes header
+    extern struct drm_display_mode fbkms_preferred_mode;
     return fbkms_get_modes(connector, &fbkms_preferred_mode);
 }
-
 
 static const struct drm_connector_funcs fbkms_conn_funcs = {
     .fill_modes = drm_helper_probe_single_connector_modes,
