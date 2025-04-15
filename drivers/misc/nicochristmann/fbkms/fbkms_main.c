29f62abb5fe6f9e53d20f916c36cce093ab6bf4a
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-15 10:57:11 +0200

    Update fbkms_main.c

--- drivers/misc/nicochristmann/fbkms/fbkms_main.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_main.c
@@ -85,7 +85,22 @@ static int fbkms_probe(struct platform_device *pdev)
 
     fbkms->pipe.connector->funcs = &fbkms_connector_funcs;
 
-    drm_connector_helper_add(&fbkms->pipe.connector, &fbkms_conn_helper_funcs);
+    struct drm_connector *conn = &fbkms->pipe.connector;
+
+    conn->display_info.width_mm = 68;
+    conn->display_info.height_mm = 122;
+    conn->polled = DRM_CONNECTOR_POLL_HPD;
+
+    ret = drm_connector_init(&fbkms->drm, conn, &fbkms_connector_funcs,
+                         DRM_MODE_CONNECTOR_VIRTUAL);
+    if (ret)
+        return ret;
+
+drm_connector_helper_add(conn, &fbkms_conn_helper_funcs);
+
+ret = drm_connector_register(conn);
+if (ret)
+    return ret;
 
     drm_mode_config_reset(&fbkms->drm);
 
