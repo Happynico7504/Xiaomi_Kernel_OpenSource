67cc15e6f469d6cd75bd9704c855ce9f774d9558
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-04-22 13:28:12 +0200

    Update fbkms_connector_helpers.h

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
@@ -1,11 +1,6 @@
 #ifndef FBKMS_CONNECTOR_HELPERS_H
 #define FBKMS_CONNECTOR_HELPERS_H
 
-#include <drm/drmP.h>
-#include <drm/drm_fb_helper.h>
-#include <drm/drm_crtc_helper.h>
-#include <drm/drm_gem_cma_helper.h>
-#include <drm/drm_simple_kms_helper.h>
 #include <drm/drm_connector.h>
 #include <drm/drm_modes.h>
 
