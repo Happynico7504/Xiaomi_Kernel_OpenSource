fba9425702d7cd98f475f3e0ab43dec90eafe169
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 21:53:17 +0200

    Update fbkms_types.h

--- drivers/misc/nicochristmann/fbkms/fbkms_types.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_types.h
@@ -1,15 +1,6 @@
-#ifndef FBKMS_TYPES_H
-#define FBKMS_TYPES_H
-
-#include <drm/drm_device.h>
-#include <drm/drm_simple_kms_helper.h>
-#include <linux/fb.h>
-
 struct fbkms_device {
     struct drm_device drm;
     struct drm_simple_display_pipe pipe;
     struct drm_display_mode mode;
     struct fb_info *fb;
 };
-
-#endif /* FBKMS_TYPES_H */
