3846be81f38e5a25825cacd2aa8c570fa2053d07
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 16:44:14 +0200

    Update fbkms_connector_helpers.h

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
@@ -10,8 +10,6 @@ const struct drm_connector_funcs fbkms_conn_funcs = {
     .reset = drm_atomic_helper_connector_reset,
     .fill_modes = drm_helper_probe_single_connector_modes,
     .destroy = drm_connector_cleanup,
-    .atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
-    .atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
 const struct drm_connector_helper_funcs fbkms_conn_helper_funcs = {
