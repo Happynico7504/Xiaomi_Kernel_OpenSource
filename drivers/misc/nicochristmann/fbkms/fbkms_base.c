f0dfba93bbfd79ca8518a149fdfe5052b19dccab
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 21:22:29 +0200

    Update and rename fbkms_main.c to fbkms_base.c

--- drivers/misc/nicochristmann/fbkms/fbkms_base.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_base.c
@@ -7,9 +7,7 @@
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_simple_kms_helper.h>
 
-#include "fbkms_connector_helpers.h"
-#include "fbkms_output_modes.h"
-#include "fbkms_types.h"
+#include "fbkms_sdk.h"
 
 static void fbkms_pipe_enable(struct drm_simple_display_pipe *pipe,
                               struct drm_crtc_state *crtc_state,
@@ -117,53 +115,6 @@ static int fbkms_probe(struct platform_device *pdev)
         conn->display_info.height_mm = 122;
         conn->polled = DRM_CONNECTOR_POLL_HPD;
 
-    ret = drm_dev_register(&fbkms->drm, 0);
-    if (ret) {
-        dev_err(&pdev->dev, "fbkms: drm_dev_register failed (%d)\n", ret);
-        goto err_pipe;
-    }
-    pr_info("fbkms: drm_dev_register success\n");
-
-    drm_kms_helper_poll_init(&fbkms->drm);
-    pr_info("fbkms: KMS poll init done\n");
-
-    dev_info(&pdev->dev, "fbkms driver registered successfully\n");
-    return 0;
-
-err_pipe:
-    drm_mode_config_cleanup(&fbkms->drm);
-err_config:
-    drm_dev_put(&fbkms->drm);
-    return ret;
-}
-
-static int fbkms_remove(struct platform_device *pdev)
-{
-    struct fbkms_device *fbkms = platform_get_drvdata(pdev);
-    pr_info("fbkms: remove called\n");
-
-    drm_kms_helper_poll_fini(&fbkms->drm);
-    pr_info("fbkms: shutting down polling\n");
-
-    drm_dev_unregister(&fbkms->drm);
-    pr_info("fbkms: drm_dev_unregister done\n");
-
-    drm_mode_config_cleanup(&fbkms->drm);
-    pr_info("fbkms: drm_mode_config_cleanup done\n");
-
-    drm_dev_put(&fbkms->drm);
-    pr_info("fbkms: drm_dev_put done\n");
-
-    return 0;
-}
-
-static struct platform_driver fbkms_platform_driver = {
-    .probe = fbkms_probe,
-    .remove = fbkms_remove,
-    .driver = {
-        .name = "fbkms",
-    },
-};
 
 static int __init fbkms_init(void)
 {
