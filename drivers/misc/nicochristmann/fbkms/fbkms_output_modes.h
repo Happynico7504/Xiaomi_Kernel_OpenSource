2d478931031cf9dade35772ab2be2274ea01507c
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 19:15:29 +0200

    Update fbkms_output_modes.h

--- drivers/misc/nicochristmann/fbkms/fbkms_output_modes.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_output_modes.h
@@ -5,6 +5,16 @@
 
 #include <drm/drm_modes.h>
 
+struct drm_display_mode fbkms_preferred_mode = {
+    .clock = 71000,
+    .hdisplay = 720,
+    .vdisplay = 1600,
+    .vrefresh = 60,
+    .flags = DRM_MODE_FLAG_NHSYNC | DRM_MODE_FLAG_NVSYNC,
+    .type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED,
+    .name = "720x1600"
+};
+
 void fbkms_setup_mode(struct fbkms_device *fbkms)
 {
     fbkms->mode = fbkms_preferred_mode;
