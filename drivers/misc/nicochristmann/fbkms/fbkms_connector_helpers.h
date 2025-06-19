df0baf60aa2fd6b5b527e33834b22f7db7f0fa10
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 18:16:29 +0200

    Update fbkms_connector_helpers.h

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
@@ -3,6 +3,9 @@
 
 #include <drm/drm_connector.h>
 
+int fbkms_get_modes(struct drm_connector *connector, struct drm_display_mode *preferred_mode);
+int fbkms_get_modes_wrapper(struct drm_connector *connector);
+
 const struct drm_connector_funcs fbkms_conn_funcs = {
     .fill_modes = drm_helper_probe_single_connector_modes,
     .destroy = drm_connector_cleanup,
