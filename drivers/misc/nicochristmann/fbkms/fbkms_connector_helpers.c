1ad1a73c0f946ad56a81b8aa613a283c66de8dc6
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 21:16:44 +0200

    Update fbkms_connector_helpers.c

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.c
@@ -3,25 +3,3 @@
 #include <drm/drm_atomic_helper.h>
 #include "fbkms_connector_helpers.h"
 #include "fbkms_output_modes.h"
-
-int fbkms_get_modes(struct drm_connector *connector, struct drm_display_mode *preferred_mode)
-{
-    pr_info("loading modes\n");
-
-    struct drm_display_mode *mode = drm_mode_duplicate(connector->dev, preferred_mode);
-    if (!mode)
-        return 0;
-
-    mode->type |= DRM_MODE_TYPE_PREFERRED;
-    drm_mode_probed_add(connector, mode);
-
-    return 1;
-}
-
-int fbkms_get_modes_wrapper(struct drm_connector *connector)
-{
-    pr_info("prepare for mode loading\n");
-
-    extern struct drm_display_mode fbkms_preferred_mode;
-    return fbkms_get_modes(connector, &fbkms_preferred_mode);
-}
