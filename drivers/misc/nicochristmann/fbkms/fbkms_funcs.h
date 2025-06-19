8a9b03417d82947305239c06700dd09c7a6f964c
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 21:27:26 +0200

    Update fbkms_funcs.h

--- drivers/misc/nicochristmann/fbkms/fbkms_funcs.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_funcs.h
@@ -1,4 +1,110 @@
+static void fbkms_pipe_enable(struct drm_simple_display_pipe *pipe,
+                              struct drm_crtc_state *crtc_state,
+                              struct drm_plane_state *plane_state)
+{
+  
+    struct fbkms_device *fbkms = container_of(pipe->crtc.dev, struct fbkms_device, drm);
 
+    struct drm_framebuffer *fb = plane_state->fb;
+
+    struct drm_gem_cma_object *cma_obj = to_drm_gem_cma_obj(fb->obj[0]);
+    void *src = cma_obj->vaddr;
+
+    memcpy(fbkms->fb->screen_base, src,
+           fb->height * fb->pitches[0]);
+
+    pr_info("fbkms pipe enabled\n");
+  
+}
+
+static void fbkms_pipe_disable(struct drm_simple_display_pipe *pipe)
+{
+    pr_info("fbkms pipe disabled\n");
+}
+
+static const struct drm_simple_display_pipe_funcs fbkms_pipe_funcs = {
+    .enable = fbkms_pipe_enable,
+    .disable = fbkms_pipe_disable,
+};
+
+static const uint32_t fbkms_formats[] = {
+    DRM_FORMAT_XRGB8888,
+};
+
+static struct drm_driver fbkms_driver = {
+    .driver_features = DRIVER_MODESET | DRIVER_GEM,
+    .name = "fbkms",
+    .desc = "Framebuffer KMS",
+    .date = "20250619",
+    .gem_free_object_unlocked = drm_gem_cma_free_object,
+    .dumb_create = drm_gem_cma_dumb_create,
+    .dumb_destroy = drm_gem_dumb_destroy,
+};
+
+static int fbkms_probe(struct platform_device *pdev)
+{
+    struct fbkms_device *fbkms;
+    int ret;
+
+    pr_info("fbkms: probe started\n");
+
+    fbkms = devm_kzalloc(&pdev->dev, sizeof(*fbkms), GFP_KERNEL);
+    if (!fbkms) {
+        dev_err(&pdev->dev, "fbkms: failed to allocate device struct\n");
+        return -ENOMEM;
+    }
+
+    fbkms->fb = registered_fb[0];
+    if (!fbkms->fb || !fbkms->fb->screen_base) {
+        dev_err(&pdev->dev, "fbkms: fb0 not available or screen_base null\n");
+        return -ENODEV;
+    }
+
+    platform_set_drvdata(pdev, fbkms);
+    pr_info("fbkms: platform data set\n");
+
+    fbkms->drm.dev = &pdev->dev;
+    ret = drm_dev_init(&fbkms->drm, &fbkms_driver, &pdev->dev);
+    if (ret) {
+        dev_err(&pdev->dev, "fbkms: drm_dev_init failed (%d)\n", ret);
+        return ret;
+    }
+    pr_info("fbkms: drm_dev_init successful\n");
+
+    if (!fbkms->drm.dev) {
+      pr_err("fbkms: drm.dev is NULL!\n");
+      return -EINVAL;
+    }
+
+    pr_info("fbkms: drm.dev = %px\n", &fbkms->drm);
+    pr_info("fbkms: drm.dev.dev = %px\n", fbkms->drm.dev);
+
+    drm_mode_config_init(&fbkms->drm);
+    pr_info("fbkms: drm_mode_config_init done\n");
+
+    fbkms_setup_mode(fbkms);
+    pr_info("fbkms: mode setup done\n");
+
+    ret = drm_simple_display_pipe_init(&fbkms->drm,
+        &fbkms->pipe,
+        &fbkms_pipe_funcs,
+        fbkms_formats, ARRAY_SIZE(fbkms_formats),
+        &fbkms_conn_funcs,
+        &fbkms_conn_helper_funcs);
+
+    
+    if (ret) {
+        dev_err(&pdev->dev, "fbkms: drm_simple_display_pipe_init failed (%d)\n", ret);
+        goto err_config;
+    }
+    pr_info("fbkms: display pipe init done\n");
+
+    struct drm_connector *conn = fbkms->pipe.connector;
+        conn->display_info.width_mm = 68;
+        conn->display_info.height_mm = 122;
+        conn->polled = DRM_CONNECTOR_POLL_HPD;
+
+    
     ret = drm_dev_register(&fbkms->drm, 0);
     if (ret) {
         dev_err(&pdev->dev, "fbkms: drm_dev_register failed (%d)\n", ret);
