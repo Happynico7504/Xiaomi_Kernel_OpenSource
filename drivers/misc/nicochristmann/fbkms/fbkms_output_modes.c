b1ca47de4c71d910179f30fd9fa9196e786f5e6d
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 15:34:53 +0200

    Update fbkms_output_modes.c

--- drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
@@ -12,3 +12,8 @@ struct drm_display_mode fbkms_preferred_mode = {
     .type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED,
     .name = "720x1600"
 };
+
+void fbkms_setup_mode(struct fbkms_device *fbkms)
+{
+    fbkms->mode = fbkms_preferred_mode;
+}
