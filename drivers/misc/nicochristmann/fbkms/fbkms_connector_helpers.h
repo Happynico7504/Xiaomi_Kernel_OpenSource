f02d634dc9096ff89aa2171cfa103bf2eb48f1e0
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 15:15:53 +0200

    Update fbkms_connector_helpers.h

--- drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_connector_helpers.h
@@ -1,45 +1,12 @@
 #ifndef FBKMS_CONNECTOR_HELPERS_H
 #define FBKMS_CONNECTOR_HELPERS_H
 
-#include <drm/drm_atomic_helper.h>
 #include <drm/drm_connector.h>
-#include <drm/drm_modes.h>
 
-static inline int fbkms_get_modes(struct drm_connector *connector,
-                                  struct drm_display_mode *preferred_mode)
-{
+int fbkms_get_modes(struct drm_connector *connector, struct drm_display_mode *preferred_mode);
+int fbkms_get_modes_wrapper(struct drm_connector *connector);
 
-    pr_info("loading modes\n");
-
-    struct drm_display_mode *mode = drm_mode_duplicate(connector->dev, preferred_mode);
-    if (!mode)
-        return 0;
-
-    mode->type |= DRM_MODE_TYPE_PREFERRED;
-    drm_mode_probed_add(connector, mode);
-
-    return 1;
-}
-
-static int fbkms_get_modes_wrapper(struct drm_connector *connector)
-{
-    pr_info("prepare for mode loading\n");
-  
-    extern struct drm_display_mode fbkms_preferred_mode;
-    return fbkms_get_modes(connector, &fbkms_preferred_mode);
-}
-
-static const struct drm_connector_funcs fbkms_connector_funcs = {
-    .reset = drm_atomic_helper_connector_reset,
-    .fill_modes = drm_helper_probe_single_connector_modes,
-    .destroy = drm_connector_cleanup,
-    .atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
-    .atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
-};
-
-static const struct drm_connector_helper_funcs fbkms_conn_helper_funcs = {
-    .get_modes = fbkms_get_modes_wrapper,
-};
-
+extern const struct drm_connector_funcs fbkms_conn_funcs;
+extern const struct drm_connector_helper_funcs fbkms_conn_helper_funcs;
 
 #endif // FBKMS_CONNECTOR_HELPERS_H
