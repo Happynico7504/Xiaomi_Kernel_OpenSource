44b4d57756a545e4eeb9b68400da89a42bea9326
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 21:49:41 +0200

    Create fbkms_headers.h

---
+++ drivers/misc/nicochristmann/fbkms/fbkms_headers.h
@@ -0,0 +1,7 @@
+#include <linux/platform_device.h>
+#include <linux/fb.h>
+#include <drm/drmP.h>
+#include <drm/drm_fb_helper.h>
+#include <drm/drm_crtc_helper.h>
+#include <drm/drm_gem_cma_helper.h>
+#include <drm/drm_simple_kms_helper.h>
