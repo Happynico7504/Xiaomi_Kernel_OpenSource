e60cb3b101966ebba74da20292d68d0e5a88526a
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 19:17:22 +0200

    Update fbkms_output_modes.c

--- drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_output_modes.c
@@ -2,3 +2,6 @@
 
 #include <drm/drm_modes.h>
 #include "fbkms_output_modes.h"
+
+int fbkms_get_modes(struct drm_connector *connector, struct drm_display_mode *preferred_mode);
+int fbkms_get_modes_wrapper(struct drm_connector *connector);
