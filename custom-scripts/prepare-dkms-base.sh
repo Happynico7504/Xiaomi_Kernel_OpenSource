dd5859aeed302aa96e080334e9616978edcba856
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-20 00:09:49 +0200

    Update prepare-dkms-base.sh

--- custom-scripts/prepare-dkms-base.sh
+++ custom-scripts/prepare-dkms-base.sh
@@ -2,4 +2,4 @@
 
 mkdir dkms-base
 
-cp -rP * dkms-base
+cp -rP arch block certs crypto drivers firmware fs include init ipc kernel lib mm modules net samples scripts security sound tools usr virt Makefile Kconfig Kbuild dkms-base
