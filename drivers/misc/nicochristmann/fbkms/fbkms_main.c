153ebb395ffa42340e82db4269c06db4cb444a2b
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-19 14:22:00 +0200

    Update fbkms_main.c

--- drivers/misc/nicochristmann/fbkms/fbkms_main.c
+++ drivers/misc/nicochristmann/fbkms/fbkms_main.c
@@ -116,10 +116,11 @@ static int fbkms_probe(struct platform_device *pdev)
     pr_info("fbkms: mode setup done\n");
 
     ret = drm_simple_display_pipe_init(&fbkms->drm, &fbkms->pipe,
-                                       &fbkms_pipe_funcs, fbkms_formats,
-                                       ARRAY_SIZE(fbkms_formats),
-                                       NULL,
-                                       &fbkms->pipe.connector);
+                                   &fbkms_pipe_funcs, fbkms_types,
+                                   ARRAY_SIZE(fbkms_formats),
+                                   &fbkms_conn_funcs,
+                                   &fbkms->pipe.connector);
+    
     if (ret) {
         dev_err(&pdev->dev, "fbkms: drm_simple_display_pipe_init failed (%d)\n", ret);
         goto err_config;
