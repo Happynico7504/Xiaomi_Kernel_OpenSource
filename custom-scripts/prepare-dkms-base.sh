3ee9e9d31ab74fc2637c117e03cd09387a41a17d
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-23 11:53:04 +0200

    Update prepare-dkms-base.sh

--- custom-scripts/prepare-dkms-base.sh
+++ custom-scripts/prepare-dkms-base.sh
@@ -2,6 +2,6 @@
 
 mkdir dkms-base
 
-cp -rP arch block certs crypto drivers firmware fs include init ipc kernel lib mm modules net samples scripts security sound tools usr virt Makefile Kconfig Kbuild dkms-base
+cp -rP arch block certs crypto drivers firmware fs include init ipc kernel lib mm modules net samples scripts security sound tools usr virt Makefile Kconfig Kbuild build-out dkms-base
 
 tar -c dkms-base | gzip -9 > dkms-base.tar.gz
