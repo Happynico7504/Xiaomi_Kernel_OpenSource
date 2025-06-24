0485eef75ead55af0f81678677563c708ebb3dd5
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-24 15:37:47 +0200

    Update mtkfb.c

--- drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
@@ -654,8 +654,9 @@ static int mtkfb_pan_display_impl(struct fb_var_screeninfo *var,
 	session_input->setter = SESSION_USER_PANDISP;
 
 	// make font visible
-	session_input->config[1].layer_id =
+	session_input->config[1].layer_id = 99
 	primary_display_get_option("ASSERT_LAYER");
+	session_input->src_fmt = DISP_FORMAT_RGBA8888;
 	session_input->config[1].next_buff_idx = -1;
 	session_input->config[1].layer_enable = 1;
         session_input->config_layer_num++;
