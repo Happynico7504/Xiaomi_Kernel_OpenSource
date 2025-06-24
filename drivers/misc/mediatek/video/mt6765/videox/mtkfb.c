dca8786c9fb8803ad810c3f7b7c6247c62ea05ee
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-24 14:07:50 +0200

    Update mtkfb.c

--- drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6765/videox/mtkfb.c
@@ -629,11 +629,11 @@ static int mtkfb_pan_display_impl(struct fb_var_screeninfo *var,
 		break;
 	case 32:
 		if (var->red.offset == 0 && var->green.offset == 8 && var->blue.offset == 16 && var->transp.offset == 24)
-                    fb_layer.src_fmt = MTK_FB_FORMAT_RGBA8888;
+                    fb_layer.src_fmt = DISP_FORMAT_RGBA8888;
                 else if (var->blue.offset == 0)
-                    fb_layer.src_fmt = MTK_FB_FORMAT_ARGB8888;
+                    fb_layer.src_fmt = DISP_FORMAT_ARGB8888;
                 else
-                    fb_layer.src_fmt = MTK_FB_FORMAT_BGRA8888;
+                    fb_layer.src_fmt = DISP_FORMAT_BGRA8888;
 		break;
 	default:
 		DISPWARN("Invalid color format bpp: %d\n", var->bits_per_pixel);
