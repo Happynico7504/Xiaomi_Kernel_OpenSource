39bde43f1f8f23dc34968c498781f24d3111c8f6
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-26 01:10:00 +0200

    Update fbkms_base.h

--- drivers/misc/nicochristmann/fbkms/fbkms_base.h
+++ drivers/misc/nicochristmann/fbkms/fbkms_base.h
@@ -20,7 +20,7 @@ int fbkms_get_modes(struct drm_connector *connector, struct drm_display_mode *ou
 
     drm_mode_debug_printmodeline(output_mode);
 
-    pr_info("output_mode name: %s\n", mode->name);
+    pr_info("output_mode name: %s\n", output_mode->name);
 
     pr_info("adding mode to connector\n");
     drm_mode_probed_add(connector, output_mode);
