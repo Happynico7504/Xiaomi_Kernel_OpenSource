44351d0dd5bb2a461e47e7f8d26dd20584d115ba
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-06-27 12:21:21 +0200

    Update pstore_rawblk.c

--- drivers/misc/nicochristmann/pstore_rawblk/pstore_rawblk.c
+++ drivers/misc/nicochristmann/pstore_rawblk/pstore_rawblk.c
@@ -96,7 +96,7 @@ static int raw_pstore_read(struct pstore_record *record)
     if (!bh)
         return -EIO;
 
-    record->type = PSTORE_TYPE_DMESG;
+    record->type = PSTORE_TYPE_CONSOLE;
     record->size = PSTORE_BLOCK_SIZE;
     record->buf = kmemdup(bh->b_data, PSTORE_BLOCK_SIZE, GFP_KERNEL);
     record->time = ns_to_timespec64(ktime_get_real_ns());
