1ac2d06527dd42f1ea31181646c21322c435f0fa
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-08-08 16:22:29 +0200

    Delete drivers/misc/nicochristmann/fbkms/fbkms_types.h

--- drivers/misc/nicochristmann/fbkms/fbkms_types.h
+++
@@ -1,7 +0,0 @@
-struct fbkms_device {
-    struct drm_device drm;
-    struct drm_simple_display_pipe pipe;
-    struct drm_display_mode mode;
-    struct drm_connector connector;
-    struct fb_info *fb;
-};
