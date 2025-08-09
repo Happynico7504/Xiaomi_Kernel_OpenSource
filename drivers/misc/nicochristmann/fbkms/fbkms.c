94940b3a7a08161ae90d486dceb964fabd4ababc
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-08-09 15:09:08 +0200

    Update fbkms.c

--- drivers/misc/nicochristmann/fbkms/fbkms.c
+++ drivers/misc/nicochristmann/fbkms/fbkms.c
@@ -15,7 +15,6 @@
 #include <drm/drm_print.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_modeset_helper.h>
-#include <drm/drm_kms_helper.h>
 #include <drm/drm_connector.h>
 #include <drm/drm_simple_kms_helper.h>
 
