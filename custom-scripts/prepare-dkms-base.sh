bccaa0d3dc194337639d4676d1ef6c25124cabf2
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-11-08 21:07:27 +0100

    Delete custom-scripts/prepare-dkms-base.sh

--- custom-scripts/prepare-dkms-base.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir dkms-base
-
-cp -rP arch block certs crypto drivers firmware fs include init ipc kernel lib mm modules net samples scripts security sound tools usr virt Makefile Kconfig Kbuild build-out dkms-base
-
-tar -c dkms-base | gzip -9 > dkms-base.tar.gz
