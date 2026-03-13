357430d14001a368719ce432c9dedaf7d1e2b5f1
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2026-03-13 13:24:49 +0100

    Enhance vkms_output with last frame info and exports
    Added fields for last frame information in vkms_output structure and exported framebuffer variables.

--- drivers/gpu/drm/vkms/vkms_drv.h
+++ drivers/gpu/drm/vkms/vkms_drv.h
@@ -1,5 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
-
 #ifndef _VKMS_DRV_H_
 #define _VKMS_DRV_H_
 
@@ -19,43 +18,49 @@
 #define YRES_MAX  8192
 
 static const u32 vkms_formats[] = {
-	DRM_FORMAT_XRGB8888,
+    DRM_FORMAT_XRGB8888,
 };
 
 struct vkms_output {
-	struct drm_crtc crtc;
-	struct drm_encoder encoder;
-	struct drm_connector connector;
-	struct hrtimer vblank_hrtimer;
-	ktime_t period_ns;
-	struct drm_pending_vblank_event *event;
+    struct drm_crtc crtc;
+    struct drm_encoder encoder;
+    struct drm_connector connector;
+    struct hrtimer vblank_hrtimer;
+    ktime_t period_ns;
+    struct drm_pending_vblank_event *event;
+
+    /* patched: last frame info */
+    void *last_framebuffer;
+    u32 last_width;
+    u32 last_height;
+    u32 last_pitch;
 };
 
 struct vkms_device {
-	struct drm_device drm;
-	struct platform_device *platform;
-	struct vkms_output output;
+    struct drm_device drm;
+    struct platform_device *platform;
+    struct vkms_output output;
 };
 
 struct vkms_gem_object {
-	struct drm_gem_object gem;
-	struct mutex pages_lock; /* Page lock used in page fault handler */
-	struct page **pages;
+    struct drm_gem_object gem;
+    struct mutex pages_lock; /* Page lock used in page fault handler */
+    struct page **pages;
 };
 
 #define drm_crtc_to_vkms_output(target) \
-	container_of(target, struct vkms_output, crtc)
+ container_of(target, struct vkms_output, crtc)
 
 #define drm_device_to_vkms_device(target) \
-	container_of(target, struct vkms_device, drm)
+ container_of(target, struct vkms_device, drm)
 
 /* CRTC */
 int vkms_crtc_init(struct drm_device *dev, struct drm_crtc *crtc,
-		   struct drm_plane *primary, struct drm_plane *cursor);
+     struct drm_plane *primary, struct drm_plane *cursor);
 
 bool vkms_get_vblank_timestamp(struct drm_device *dev, unsigned int pipe,
-			       int *max_error, ktime_t *vblank_time,
-			       bool in_vblank_irq);
+          int *max_error, ktime_t *vblank_time,
+          bool in_vblank_irq);
 
 int vkms_output_init(struct vkms_device *vkmsdev);
 
@@ -65,11 +70,17 @@ struct drm_plane *vkms_plane_init(struct vkms_device *vkmsdev);
 int vkms_gem_fault(struct vm_fault *vmf);
 
 int vkms_dumb_create(struct drm_file *file, struct drm_device *dev,
-		     struct drm_mode_create_dumb *args);
+       struct drm_mode_create_dumb *args);
 
 int vkms_dumb_map(struct drm_file *file, struct drm_device *dev,
-		  u32 handle, u64 *offset);
+    u32 handle, u64 *offset);
 
 void vkms_gem_free_object(struct drm_gem_object *obj);
+
+/* Exported framebuffer for fbdev bridge */
+extern void *vkms_last_framebuffer;
+extern u32 vkms_last_width;
+extern u32 vkms_last_height;
+extern u32 vkms_last_pitch;
 
 #endif /* _VKMS_DRV_H_ */
